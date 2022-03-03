18cee4b31a483fd049933d0a77aa32d045e88f80
kiranbhatte <kiranbhatte007@gmail.com>
2022-03-03 23:17:02 +0530

    code changes for shuffle and sort

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-let number_wrapper = document.getElementById('number-wrapper');
-numbers.map((num) => {
-    let ele = document.createElement('div')
-    ele.classList.add(`color-${num}`)
-    ele.innerText = num
-    number_wrapper.appendChild(ele)
-})
-const shuffle = () => {
-    // Shuffle logic
-    // console.log(numbers.sort(() => Math.random() - 0.5))
+// append element to Dom 
+const set_board = (numbers) => {
+    let number_wrapper = document.getElementById('number-wrapper');
+    // reset number wrapper before using
+    number_wrapper.innerHTML = ""
+    numbers.map((num) => {
+        let ele = document.createElement('div')
+        ele.classList.add(`color-${num}`)
+        ele.innerText = num
+        number_wrapper.appendChild(ele)
+    })
 }
+// default board numbers
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// Intial page data set method
+set_board(numbers);
+
+// shuffle default board numbers
+const shuffle = () => {
+    // Shuffle default board numbers
+    let nums = []
+    numbers.map(element => {
+        nums.push(element)
+    });
+    nums = nums.sort(() => Math.random() - 0.5)
+    // put shuffle data
+    set_board(nums)
+}
+// sort default board numbers
 const short = () => {
-    // Short logic
+    set_board(numbers)
 }
